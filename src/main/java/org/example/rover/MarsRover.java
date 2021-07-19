abbb7f804a40f9e1d8e7987658461cb62978380a
Torfs Jellis <torfsje@cronos.be>
2021-07-19 10:22:57 +0200

    initial commit

---
+++ src/main/java/org/example/rover/MarsRover.java
@@ -0,0 +1,7 @@
+package org.example.rover;
+
+public class MarsRover {
+    public MarsRover(int x, int y, char orientation) {
+
+    }
+}
---
+++ src/test/java/org/example/rover/MarsRoverTest.java
@@ -0,0 +1,13 @@
+package org.example.rover;
+
+import org.junit.jupiter.api.Test;
+
+class MarsRoverTest {
+
+    @Test
+    void whenCreatingARoverWithInitialPositionAndOrientation_shouldCreateRoverOnThatPositionAndOrientation() {
+        MarsRover rover = new MarsRover(1, 4, 'N');
+
+        //TODO add assertions
+    }
+}
